29ceeca90ea64b678ab5c8a608272c70d2293004
italo <italorpereira0@gmail.com>
2022-07-09 18:47:34 -0300

    jogo da forca funcionando

---
+++ JS/jogo-forca.js
@@ -0,0 +1,92 @@
+var palavras = ["POXA","VIDA","CARRINHO"];
+var tela = document.getElementById("tela").getContext("2d");
+var letras = [];
+var palavraCorreta = "";
+var erros = 9;
+
+function textCorreto(){
+    var palavra = palavras[Math.floor(Math.random()*palavras.length)];
+    palavraSecreta = palavra;
+    console.log(palavra);
+    return palavra;
+}
+
+function tracos(){
+    tela.lineWidth = 6;
+    tela.lineCap = "round";
+    tela.lineJoin = "round";
+    tela.strokeStyle = "#0A3871";
+    tela.beginPath();
+    var eixo = 600/palavraSecreta.length;
+    for(let i = 0; i < palavraSecreta.length;i++){
+        tela.moveTo(500+(eixo*i),640);
+        tela.lineTo(550+(eixo*i),640);
+    }
+    tela.stroke();
+    tela.closePath();
+}
+
+tracos(textCorreto());
+
+function letraCorreta(index) {
+    tela.font = "bold 52px Montserrat";
+    tela.lineWidth = 6;
+    tela.lineCap = "round";
+    tela.lineJoin = "round";
+    tela.strokeStyle = "#0A3871";
+
+    var eixo = eixo = 600/palavraSecreta.length;
+    tela.fillText(palavraSecreta[index],505+(eixo*index),620);
+    tela.stroke()
+}
+
+function letraIncorreta(letra,errosLeft){
+    tela.font = "bold 40px Montserrat";
+    tela.lineWidth = 6;
+    tela.lineCap = "round";
+    tela.lineJoin = "round";
+    tela.strokeStyle = "#0A3871";
+    tela.fillText(letra,535+(40*(10-errosLeft)),710,40);
+}
+
+function verificarLetraCorreta(key){
+    if(letras.length < 1 || letras.indexOf(key) < 0){
+        console.log(key);
+        letras.push(key);
+        return false;
+    }else{
+        letras.push(key.toUpperCase());
+        return true
+    }
+}
+
+function addLetraCorreta(i){
+    palavraCorreta += palavraSecreta[i].toUpperCase;
+}
+
+function addletraIncorreta(i){
+    if(palavraSecreta.indexOf(i)<= 0 ){
+        erros -= 1;
+    }
+}
+
+document.onkeydown = (e) => {
+    var letra = e.key.toUpperCase();
+    if(!verificarLetraCorreta(e.key)){
+        if(palavraSecreta.includes(letra)){
+            addLetraCorreta(palavraSecreta.indexOf(letra));
+            for(let i = 0; i < palavraSecreta.length;i++){
+                if(palavraSecreta[i] === letra){
+                    letraCorreta(i);
+                }
+            }
+        }else{
+            if(!verificarLetraCorreta(e.key))
+            return
+            addletraIncorreta(letra);
+            letraIncorreta(letra,erros);
+        }
+    }
+
+
+}
